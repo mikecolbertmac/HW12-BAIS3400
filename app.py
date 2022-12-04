11762ca4c663ed39bea7f5147c0eccce0d292feb
Mike Colbert <mikec@Mikes-MacBook-Pro-2.local>
2022-12-03 19:56:16 -0600

    updated MySQL digital certificate

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ conn = pymysql.connect(
         user = _dbusername.value, 
         password = _dbpassword.value, 
         db = _dbname.value, 
-        ssl={'ca': './BaltimoreCyberTrustRoot.crt.pem'},
+        ssl={'ca': './DigiCertGlobalRootCA.crt.pem'},
         cursorclass = pymysql.cursors.DictCursor)  
 
 app = Flask(__name__)
